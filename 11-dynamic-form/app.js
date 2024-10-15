8374df4c541a58e8bf333c0dff1ee7911aa85423
kimminjeong2 <zbdj0123@naver.com>
2024-10-15 21:52:47 +0900

    10.15.2pm_study

--- 11-dynamic-form/app.js
+++ 11-dynamic-form/app.js
@@ -8,17 +8,22 @@ app.set("views", "./views");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+const userInfo = {
+  id: "banana",
+  pw: "1234",
+};
+
 app.get("/", (req, res) => {
   res.render("index");
 });
 
-//ajax get
+// ajax get
 app.get("/ajax", (req, res) => {
   console.log(req.query);
   res.send(req.query);
 });
 
-//ajax post
+// ajax post
 app.post("/ajax", (req, res) => {
   console.log(req.body);
   res.send(req.body);
@@ -37,22 +42,37 @@ app.post("/axios", (req, res) => {
   res.send(req.body);
 });
 
-//fetch get
+// fetch get
 app.get("/fetch", (req, res) => {
   console.log(req.query);
   res.json(req.query);
 });
 
-//fetch post
+// fetch post
 app.post("/fetch", (req, res) => {
   console.log(req.body);
   res.json(req.body);
 });
 
-// 실습 1
-app.get("/axios-실습1", (req, res) => {
+//////////////////
+// 실습
+app.get("/practice1", (req, res) => {
+  res.render("practice1");
+});
+
+app.get("/practice1-get", (req, res) => {
   console.log(req.query);
+  console.log(req.params);
   res.json(req.query);
+});
+
+app.post("/practice2", (req, res) => {
+  console.log(req.body);
+  if (userInfo.id === req.body.userId && userInfo.pw === req.body.userPw) {
+    res.json({ userInfo: req.body, isSuccess: true });
+  } else {
+    res.json({ isSuccess: false });
+  }
 });
 
 app.listen(PORT, () => {
