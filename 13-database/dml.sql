a6ae8cd006a2471e4a5d6b8f77a07d49a99fe724
kimminjeong2 <zbdj0123@naver.com>
2024-10-17 16:20:03 +0900

    10.17.2pm_study

--- 13-database/dml.sql
+++ 13-database/dml.sql
@@ -145,3 +145,88 @@ select sum(amount) as 'total_amount' from orders;
 -- 주문 테이블에서 총 판매 개수, 평균 판매 개수, 상품 최저가, 상품 최고가 검색
 select sum(amount) as 'total_amount', avg(amount) as 'avg_amount',
 min(price) as 'min_price', max(price) as 'max_price' from orders;
+
+-- 주문 테이블에서 총 주문 건수
+SELECT count(*) from orders;
+
+-- 주문 테이블에서 주문한 고객 수 (중복 없이)
+select count(DISTINCT custid) from orders;
+
+--  < GROUP BY >
+-- "~별로"
+
+-- 고객별로 주문한 주문 건수 구하기
+SELECT custid, count(*) from orders group by custid;
+
+-- 고객별로 주문한 상품 총 수량 구하기
+SELECT custid, sum(amount) from orders group by custid;
+
+-- 고객별로 주문한 총 주문액 구하기
+SELECT custid, sum(amount * price) from orders group by custid;
+
+-- 상품별로 판매 개수 구하기
+select prodname, sum(amount) from orders group by prodname;
+
+-- < HAVING >
+-- group by 절 이후 추가 조건
+-- 그룹에 대해서 필터링
+
+-- 총 주문액이 10000원 이상인 고객에 대해서 고객별로 주문한 상품 총 수량 구하기
+select custid, sum(amount), sum(amount * price) from orders 
+  group by custid
+  having sum(amount * price) >= 10000;
+
+  -- 총 주문액이 10000원 이상인 고객에 대해서 고객별로 주문한 상품 총 수량 구하기
+  -- (단, custid가 'bunny'인 고객은 제외하고 출력할 것)
+  -- where, group by, having 모두 사용한 예시
+select custid, sum(amount), sum(amount * price) from orders 
+  where custid != 'bunny'
+  group by custid
+  having sum(amount * price) >= 10000;
+
+  -- group by 주의 사항
+  -- select 절에서 group by 에서 사용한 속성과 집계함수만 사용가능
+
+
+-- 실습 3 CREATE
+CREATE TABLE user (
+	Id VARCHAR(10) not null primary key
+	pw VARCHAR(20) not null
+	name VARCHAR(5) not null
+	gender ENUM('F', 'M', '') DEFAULT '',
+	birthday DATE not null
+	age int(3) not null default 0
+);
+
+-- 실습 4 INSERT
+insert into user values
+  ("hong1234", "804bkg", "홍길동", "M", "1990-01-31", "33"),
+  ("sexysung", "87awjkdf", "성춘향", "F", "1992-03-31", "31"),
+  ("power70", "qxur8sda", "변사또", "M", "1970-05-02", "53"),
+  ("hanjo", "jk48fn4", "한조", "M", "1984-10-18", "39"),
+  ("widowmaker", "38ewifh3", "위도우", "F", "1976-06-27", "47"),
+  ("dvadva", "k3f3ah", "송하나", "F", "2001-06-03", "22"),
+  ("jungktat", "4ifha7f", "정크랫", "M", "1999-11-11", "24");
+select * from user;
+
+-- 실습5. SELECT
+-- 1. 모든 회원목록을 가져오는데, 이때 birthday 컬럼의 값을 기준으로 오름차순 정렬하여 가져오시오.
+select * from user order by birthday;
+-- 2. 회원 목록 중 gender 컬럼의 값이 "M" 인 회원목록을 가져오는데, 이때 name 컬럼의 값을 기준으로 내림차순 정렬하여 가져오시오.
+select * from user where gender='M' order by name desc;
+-- 3. 1990 년대에 태어난 회원의 id, name 컬럼을 가져와 목록으로 보여주시오.
+select id, name from user where birthday like '199%';
+-- 4. 6월생 회원의 목록을 birthday 기준으로 오름차순 정렬하여 가져오시오.
+select * from user where birthday like '%-06-%' order by birthday;
+-- 5. gender 컬럼의 값이 "M" 이고, 1970 년대에 태어난 회원의 목록을 가져오시오.
+select * from user where gender='M' and birthday like '197%';
+-- 6. 모든 회원목록 중 age를 기준으로 내림차순 정렬하여 가져오는데, 그때 처음 3개의 레코드만 가져오시오.
+select * from user order by age desc limit 3;
+-- 7. 모든 회원 목록 중 나이가 25 이상 50 이하인 회원의 목록을 출력하시오.
+select * from user where age between 25 and 50;
+select * from user where age >= 25 and age <= 50;
+-- 8. id 컬럼의 값이 hong1234 인 레코드의 pw 컬럼의 값을 12345678로 변경하시오.
+update user set pw='12345678' where id='hong1234';
+-- 9. id 컬럼의 값이 jungkrat인 레코드를 삭제하시오.
+delete from user where id='jungkrat'
+
