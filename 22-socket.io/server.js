47fffe506173091b1cb003fadb8ec9205c706425
kimminjeong2 <zbdj0123@naver.com>
2024-11-15 13:55:46 +0900

    24_11_15_2pm_study

---
+++ 22-socket.io/server.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const http = require("http");
+const socketIO = require("socket.io");
+
+const app = express();
+const server = http.createServer(app); // express app으로 http 서버를 생성
+const io = socketIO(server); // socket.io를 http 서버에 연결
+const PORT = 8000;
+
+app.set("view engine", "ejs");
+app.use("/static", express.static(__dirname + "/static"));
+
+app.get("/", (req, res) => {
+  res.render("chat");
+});
+
+io.on("connection", (socket) => {
+  console.log("서버 연결 완료 :: ", socket.id);
+
+  // [실습1]
+  socket.on("Hello", (data, callback) => {
+    console.log(`${data.who} : ${data.msg}`);
+    socket.emit("HelloKr", { who: "Hello", msg: "안녕!" });
+  });
+  socket.on("Study", (data, callback) => {
+    console.log(`${data.who} : ${data.msg}`);
+    socket.emit("StudyKr", { who: "Study", msg: "공부!" });
+  });
+  socket.on("Bye", (data, callback) => {
+    console.log(`${data.who} : ${data.msg}`);
+    socket.emit("ByeKr", { who: "Bye", msg: "잘가!" });
+  });
+
+  // [실습3] 채팅창 입장 문구
+  // io.emit("notice", `${socket.id} 님이 입장하셨습니다`);
+
+  // [실습3-2] 채팅창 입장 문구 socket.id -> nickname
+  socket.on("setNick", (nickname) => {
+    io.emit("notice", `${nickname} 님이 입장하셨습니다`);
+    socket.emit("entrySuccess", nickname);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`);
+});
