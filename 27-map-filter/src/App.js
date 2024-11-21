4ca618419ba5f89cff10bb096531283134cab75f
kimminjeong2 <zbdj0123@naver.com>
2024-11-21 23:55:47 +0900

    11_21_map-filter

--- 27-map-filter/src/App.js
+++ 27-map-filter/src/App.js
@@ -1,7 +1,6 @@
 import Alphabet from "./Alphabet";
 import Pr from "./Pr";
 import Pr2 from "./Pr2";
-import Pr4 from "./Pr4";
 
 function App() {
   return (
@@ -9,7 +8,6 @@ function App() {
       <Alphabet />
       <Pr />
       <Pr2 />
-      <Pr4 />
     </div>
   );
 }
